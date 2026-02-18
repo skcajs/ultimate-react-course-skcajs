e3831fc60c91b569e62add2d93fa9af60fcb9980
sam <sam.jackson@defendable.no>
2026-02-18 08:53:27 +0100

    updated

--- react-quiz/src/components/Question.js
+++ react-quiz/src/components/Question.js
@@ -2,12 +2,12 @@ import { useQuiz } from "../contexts/QuizContext";
 import Options from "./Options";
 
 function Question() {
-  const { questions, index, answer, dispatch } = useQuiz();
+  const { questions, index } = useQuiz();
   const question = questions.at(index);
   return (
     <div>
       <h4>{question.question}</h4>
-      <Options question={question} answer={answer} dispatch={dispatch} />
+      <Options question={question} />
     </div>
   );
 }
